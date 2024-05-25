f26485486bbf3abd6396c3de88c14a61410d14a5
IIU60 <hugoperezdealbeniz@gmail.com>
2024-05-25 22:49:11 +0100

    finished fitted plot

--- weibull/server.R
+++ weibull/server.R
@@ -3,26 +3,37 @@ library(shiny)
 library(ggplot2)
 library(fitdistrplus)
 
+
 # Define server logic
 server <- function(input, output) {
-  data <- reactive({
-    req(input$file)
-    read.csv(input$file$datapath)
-  })
   
-  output$weibullPlot <- renderPlot({
-    wind_speeds <- data()$wind_speed
+data <- read.csv("wind_data.csv")
+data$wind_speed <- data[['Wind.Speed..m.s.']]
+data[['Wind.Speed..m.s.']] <- NULL
+
+data$wind_speed <- replace(data$wind_speed, data$wind_speed==0, mean(data$wind_speed))
+
+output$weibullPlot <- renderPlot({
     
     # Fit Weibull distribution
-    fit <- fitdist(wind_speeds, "weibull")
-    x <- seq(0, max(wind_speeds), length.out = 100)
+    fit <- fitdist(data$wind_speed, "weibull")
+    x <- seq(0, max(data$wind_speed), length.out = 100)
     y <- dweibull(x, shape = fit$estimate["shape"], scale = fit$estimate["scale"])
     
-    ggplot(data = data.frame(x = x, y = y), aes(x = x, y = y)) +
-      geom_line() +
+    p <- ggplot(data = data.frame(x = x, y = y), aes(x = x, y = y)) +
+      geom_line(size=2) +
       labs(x = "Wind Speed (m/s)", y = "Density", title = "Wind Speed Weibull Distribution") +
-      theme_minimal() +
-      geom_histogram(data = data(), aes(x = wind_speeds, y = ..density..), bins = 30, alpha = 0.5, fill = "blue") +
-      geom_density(data = data(), aes(x = wind_speeds, y = ..density..), color = "red")
+      theme_minimal()
+    
+    # Add histogram and density layers
+    p <- p + 
+      geom_histogram(data=data, aes(x = wind_speed, y =..density..), bins = 30, alpha = 0.2, fill = "blue") +
+      geom_density(data=data, aes(x = wind_speed, y =..density..), color = "red", alpha = 0.2)
+    
+    # Manually add legends
+    p + 
+      annotate("text", x = 15, y = 0.075, label = "Blue Shade: Density Histogram") +
+      annotate("text", x = 15, y = 0.07, label = "Red Line: Density Plot") +
+      annotate("text", x = 15, y = 0.08, label = "Black Line: Fitted Weibull")
   })
 }
--- weibull/ui.R
+++ weibull/ui.R
@@ -7,8 +7,6 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      fileInput("file", "Choose CSV File:",
-                accept = c(".csv")),
       sliderInput("shape", "Shape Parameter (k):", min = 0.1, max = 5, value = 1, step = 0.1),
       sliderInput("scale", "Scale Parameter (λ):", min = 0.1, max = 50, value = 10, step = 0.1)
     ),
